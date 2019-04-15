9b1ce428f611de9f6ebecbb011ec3f47a7ad8dd6
makansari78 <mak.ansari@rjtcompuquest.com>
2019-04-16 02:45:39 +0530

    adding the code

--- app/src/main/java/com/ansari/b18fragments/FragmentOne.java
+++ app/src/main/java/com/ansari/b18fragments/FragmentOne.java
@@ -6,15 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FragmentOne extends Fragment {
 
+
+    Button passDataButton;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_one,container,false);
 
+        passDataButton = view.findViewById(R.id.buttonPassData);
+        passDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTwo fragmentTwo = new FragmentTwo();
+                Bundle bundle = new Bundle();
+                bundle.putString("key","Ansari");
+
+                fragmentTwo.setArguments(bundle);
+
+                getFragmentManager().beginTransaction().replace(R.id.mylayout,fragmentTwo).commit();
+
+
+
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/ansari/b18fragments/FragmentTwo.java
+++ app/src/main/java/com/ansari/b18fragments/FragmentTwo.java
@@ -6,14 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FragmentTwo extends Fragment {
 
+    TextView textView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_two,container,false);
+
+        textView = view.findViewById(R.id.textViewResult);
+
+        Bundle bundle = getArguments();
+        String res = bundle.getString("key");
+        textView.setText(res);
+
 
         return view;
     }
